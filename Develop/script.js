fa31b23220cbdb6b9f808d849c5c3fab1e8ec6c2
Joshua Chosay <jjchosay@gmail.com>
2021-06-11 23:25:58 -0500

    Initial commit.

---
+++ Develop/script.js
@@ -0,0 +1,57 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  //sets a blank array to hold user-defined character types
+  var characterSet = []
+
+  //accepts user input to determine inclusion of characters in characterSet
+  var charLength = prompt("How many characters would you like your new password to have?");
+
+  var useNumbers = confirm("Would you like to use numbers in your password?");
+  if (useNumbers==true){
+    characterSet.push(1,2,3,4,5,6,7,8,9,0);
+    console.log(useNumbers);
+    }   
+
+  var useUppercase = confirm("Would you like to use uppercase letters in your password?")
+  if (useUppercase==true){
+    characterSet.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+    console.log(useUppercase);
+    }
+  
+  var useLowercase = confirm("Would you like to use lowercase letters in your password?")
+  if (useLowercase==true){  
+    characterSet.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");   
+    console.log(useLowercase);
+  }
+
+  var useSpecials = confirm("Would you like to use special characters in your password?")
+  if (useSpecials == true){  
+    characterSet.push("!","@","#","%","^","&","*",":",";","~","$");
+    console.log(useSpecials);
+  }
+  //esablishes a blank password string
+  pass = "";
+
+  //loops through random selections of user-defined character types until inputted charLength value is reached
+  for(i=0; i<=charLength-1; i++){
+    var index=Math.floor(Math.random() * characterSet.length);
+    var tempSpot = characterSet[index];
+    pass+=tempSpot;
+  }
+
+  //passes newly-generated password back to writePaassword()
+  return pass; 
+
+}
